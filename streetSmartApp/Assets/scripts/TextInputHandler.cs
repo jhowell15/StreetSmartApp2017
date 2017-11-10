74b46a6278cb81ab12a17515a2e950d9a0a1ed55
williamf117 <wforrest@uccs.edu>
2017-11-10 08:16:13 -0700

    added some scripts

---
+++ streetSmartApp/Assets/scripts/TextInputHandler.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+
+
+public class TextInputHandler : MonoBehaviour {
+
+    //set up the different strings that will be set 
+    string userName;
+    float milesDriven;
+    string userEmail;
+    float milesPerWeek;
+    string userAdress;
+	
+	
+   /// <summary>
+   /// get and save the user's name 
+   /// </summary>
+   /// <param name="username"></param>
+   public void UserName(string username)
+    {
+        try
+        {
+          userName= username;
+            Debug.Log("name");
+            
+        }
+        catch {
+            
+        }
+
+    } 
+
+    /// <summary>
+    /// .takes in and saves the miles driven
+    /// </summary>
+    /// <param name="milesDrivenString"></param>
+public void MilesDriven(string milesDrivenString)
+    {
+       try
+        {
+            milesDriven = float.Parse(milesDrivenString);
+        }
+        catch
+        {
+            Debug.Log("enter a number ");
+        }
+    }
+    public void MilesPerWeek(string miles)
+    {
+        try
+        {
+            milesPerWeek = float.Parse(miles);
+        }
+        catch { Debug.Log("enter a number");
+        }
+    }
+    /// <summary>
+    /// get and save user email 
+    /// </summary>
+    /// <param name="email"></param>
+public void UserEmail(string email)
+    {
+        try
+        {
+            email = userEmail;
+        }
+        catch
+        {
+
+        }
+    }
+public void Adress(string address)
+    {
+        try
+        {
+            address = userAdress;
+        }
+        catch { }
+    }
+    /// <summary>
+    /// write all the data to a file
+    /// </summary>
+public void logIn()
+    {
+        //write to the file 
+        StreamWriter file = File.CreateText("C:\\Users\\William Forrest\\Documents\\GitHub\\StreetSmartApp2017\\streetSmartApp\\logInInfo.txt");
+        file.WriteLine(userAdress+","+userEmail + "," +userName + "," +milesDriven + "," +milesPerWeek);
+        file.Close();
+    }
+
+}
+
---
+++ streetSmartApp/Assets/scripts/UserDataSheet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class UserDataSheet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        
+        
+            StreamReader input = null;
+        //chech to see if the user has already loged in 
+        try
+        {
+            input = File.OpenText("logInInfo.txt");
+            input.Close();
+        }
+        catch
+        {   
+            //create the data file if it dose not already exist 
+            StreamWriter logeInInfo = File.CreateText("logInInfo.txt");
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
