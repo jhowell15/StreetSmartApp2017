4738937c0a7757ab68426f96db38dcb5dd760188
TheAVIATOR559 <howelljacob05@gmail.com>
2017-11-27 15:01:44 -0700

    *READ DESCRIPTION* Maintenance Email Reminder
    The code should work but I cant verify until the app is on android.

--- streetSmartApp/Assets/scripts/MoveToSceneTimer.cs
+++ streetSmartApp/Assets/scripts/MoveToSceneTimer.cs
@@ -18,8 +18,40 @@ public class MoveToSceneTimer : MonoBehaviour
         if(time <= 0.0f)
         {
             timerEnded();
+            //EmailReminder();
         }
 	}
+
+    private void EmailReminder()
+    {
+        string emailAddress;
+        StreamReader reader = new StreamReader(Application.persistentDataPath + "/loginInfo.txt");
+
+        emailAddress = reader.ReadLine();
+
+        int emailAddressStartLoc = emailAddress.IndexOf(";") + 1;
+        Debug.Log("email address pre edit: " + emailAddress);
+        emailAddress = emailAddress.Substring(emailAddressStartLoc, emailAddress.IndexOf(";", emailAddressStartLoc));
+        emailAddress = emailAddress.Substring(0, emailAddress.IndexOf(";"));
+
+        Debug.Log("email address post edit: " + emailAddress);
+
+        string emailSubject = MyEscapeURL("StreetSmart App Email Test");
+        string emailBody = MyEscapeURL("IT WORKS GOD DAMMIT!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+
+        Debug.Log("mailto:" + emailAddress + "?subject=" + emailSubject + "&body=" + emailBody);
+
+        string email = "mailto:" + emailAddress + "?subject=" + emailSubject + "&body=" + emailBody;
+        Debug.Log(email);
+
+        Application.OpenURL(email);
+
+    }
+
+    string MyEscapeURL(string url)
+    {
+        return WWW.EscapeURL(url).Replace("+", "%20");
+    }
 
     private void timerEnded()
     {
@@ -28,6 +60,7 @@ public class MoveToSceneTimer : MonoBehaviour
             StreamReader loginReader = new StreamReader(Application.persistentDataPath + "/loginInfo.txt");
             Debug.Log("file found");
             loginReader.Close();
+            EmailReminder();
             SceneManager.LoadScene(home, LoadSceneMode.Single);
 
         }
