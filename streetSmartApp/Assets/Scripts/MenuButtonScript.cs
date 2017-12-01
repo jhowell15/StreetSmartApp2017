ac1c64d9bee87cd856184bed1362238e0e8aa500
williamf117 <wforrest@uccs.edu>
2017-11-30 17:29:24 -0700

    added my comments and changed button color to mach the mockup

--- streetSmartApp/Assets/Scripts/MenuButtonScript.cs
+++ streetSmartApp/Assets/Scripts/MenuButtonScript.cs
@@ -6,21 +6,34 @@ using UnityEngine.SceneManagement;
 
 public class MenuButtonScript : MonoBehaviour
 {
+    /// <summary>
+    /// load the editvehicle info
+    /// </summary>
 	public void LoadEditVehicleInfo()
 	{
 		SceneManager.LoadScene("EditVehicleInfo", LoadSceneMode.Single);
 	}
 
+
+    /// <summary>
+    /// load the links page 
+    /// </summary>
     public void LoadLinks()
     {
         SceneManager.LoadScene("links", LoadSceneMode.Single);
     }
 
+    /// <summary>
+    /// load the login page 
+    /// </summary>
     public void LoadLogin()
     {
         SceneManager.LoadScene("login", LoadSceneMode.Single);
     }
 
+    /// <summary>
+    /// load splash screen 
+    /// </summary>
     public void LoadSplashScreen()
     {
         SceneManager.LoadScene("splashScreen", LoadSceneMode.Single);
@@ -31,6 +44,10 @@ public class MenuButtonScript : MonoBehaviour
         SceneManager.LoadScene("VehicleInfo", LoadSceneMode.Single);
     }
 
+
+    /// <summary>
+    /// quit the app 
+    /// </summary>
     public void Quit()
     {
         Application.Quit();
